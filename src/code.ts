2e16e15ae870c2431279b8a436e11bcdcd14568a
Brett Holcomb <ready.set.get.brett@gmail.com>
2024-03-20 13:05:26 -0700

    TS fixes, first pass on "create-swatches"

--- src/code.ts
+++ src/code.ts
@@ -4,19 +4,6 @@ figma.showUI(__html__, { width: 370, height: 666 });
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
 figma.ui.onmessage = (msg) => {
-  // if (msg.type === "add-colors") {
-  //   const colors: Object = figma.currentPage.selection.reduce(
-  //     (obj, item) => ({ ...obj, [item.id]: item.fills[0].color }),
-  //     {}
-  //   );
-
-  //   figma.ui.postMessage(
-  //     JSON.stringify({
-  //       msgType: "add-these-colors",
-  //       colors: colors,
-  //     })
-  //   );
-  // }
   if (msg.type === "set-fill") {
     const color = msg.color;
     const nodes = figma.currentPage.selection;
@@ -29,21 +16,29 @@ figma.ui.onmessage = (msg) => {
 
   if (msg.type === "get-fill") {
     const nodes = figma.currentPage.selection;
-    for (const node of nodes) {
-      if ("fills" in node) {
-        figma.ui.postMessage(
-          JSON.stringify({
-            msgType: "set-fill-by-id",
-            color: node.fills[0].color,
-          })
-        );
-        return;
-      }
+    const selection = figma.currentPage.selection?.[0];
+
+    if (
+      !selection ||
+      !("fills" in selection) ||
+      !Array.isArray(selection.fills) ||
+      !selection.fills[0]?.color
+    ) {
+      figma.notify("Select a layer with a solid fill to sample the color.");
+      return;
+    }
+
+    if ("fills" in selection && selection.fills?.[0]?.type === "SOLID") {
+      figma.ui.postMessage(
+        JSON.stringify({
+          msgType: "set-fill",
+          color: selection.fills[0].color,
+        })
+      );
     }
   }
 
   if (msg.type === "eye-dropper") {
-    console.log(msg);
     const id = msg.id;
     const nodes = figma.currentPage.selection;
     if (nodes.length === 0) {
@@ -51,11 +46,18 @@ figma.ui.onmessage = (msg) => {
       return;
     }
     const selected = nodes[0];
-    const color = selected?.fills?.[0].color;
-    if (!color) {
+
+    if (
+      !selected ||
+      !("fills" in selected) ||
+      !Array.isArray(selected.fills) ||
+      !selected.fills[0]?.color
+    ) {
       figma.notify("Select a layer with a solid fill to sample the color.");
       return;
     }
+    const color = selected?.fills?.[0].color;
+
     figma.ui.postMessage(
       JSON.stringify({
         msgType: "set-color-by-id",
@@ -65,5 +67,17 @@ figma.ui.onmessage = (msg) => {
     );
   }
 
-  // figma.closePlugin();
+  if (msg.type === "create-swatches") {
+    const colors = msg.colors;
+    const swatchSize = 100;
+    for (const [index, color] of colors.entries()) {
+      const rect = figma.createRectangle();
+      console.log("rect.x", rect.x);
+      rect.x = index * swatchSize;
+      rect.y = 0;
+      rect.resize(swatchSize, swatchSize);
+      rect.fills = [{ type: "SOLID", color }];
+      figma.currentPage.appendChild(rect);
+    }
+  }
 };
